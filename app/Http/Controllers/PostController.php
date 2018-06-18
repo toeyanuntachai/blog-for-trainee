7e23fe66b48e411334ebb88e3e3b3c920eda4309
boomchawarat <boom.chawarat@gmail.com>
2018-06-18 08:58:22 +0000

    // dad

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
     public function index()
     {  ////sfsdfsf sd
         /// hello
-        /// dad
+        /// boy 
         $posts = Post::with('category')->get();
 
         return view('posts.index', compact('posts'));

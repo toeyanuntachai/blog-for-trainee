ae31d99571b42745f983c566e0026a21412630b3
xxkhanxx77 <boyxcomza@hotmail.co.th>
2018-06-18 09:24:10 +0000

    hello

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,6 +10,7 @@ class PostController extends Controller
 {
     public function index()
     {
+        //hello
         $posts = Post::with('category')->get();
         return view('posts.index', compact('posts'));
     }
